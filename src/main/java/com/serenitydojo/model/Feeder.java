ca67749c17aca1be9d8a0b08e750ff06f606da72
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-02 07:19:04 +0100

    Starting point

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,6 +2,13 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
-        return "Food";
+        if (animal.equals("Cat")) {
+            return (isPremium) ? "Salmon" : "Tuna";
+        } else if (animal.equals("Dog")) {
+            return "Dog Food";
+        }
+
+        return "Cabbage";
+
     }
 }
