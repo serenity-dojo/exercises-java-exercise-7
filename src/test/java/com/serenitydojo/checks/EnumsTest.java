be2584a7f4479512c1dfa8b0322c9692e3f2ba8b
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-02 07:25:28 +0100

    Sample solution

--- src/test/java/com/serenitydojo/checks/EnumsTest.java
+++ src/test/java/com/serenitydojo/checks/EnumsTest.java
@@ -12,13 +12,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EnumsTest {
     @Test
     public void theFoodTypeEnumShouldExist() throws ClassNotFoundException {
-        assertThat(Class.forName("com.serenitydojo.FoodType")).isNotNull();
-        assertThat(Class.forName("com.serenitydojo.FoodType").isEnum()).isNotNull();
+        assertThat(Class.forName("com.serenitydojo.model.FoodType")).isNotNull();
+        assertThat(Class.forName("com.serenitydojo.model.FoodType").isEnum()).isNotNull();
     }
 
     @Test
     public void theFoodTypeEnumShouldHaveSomeValues() throws ClassNotFoundException {
-        assertThat(Class.forName("com.serenitydojo.FoodType").getEnumConstants()).isNotEmpty();
+        assertThat(Class.forName("com.serenitydojo.model.FoodType").getEnumConstants()).isNotEmpty();
     }
 
     @Test
@@ -26,6 +26,6 @@ public class EnumsTest {
         Method[] methods = Class.forName("com.serenitydojo.model.Feeder").getMethods();
         Method feedMethod = Arrays.stream(methods).filter(method -> method.getName().equals("feeds")).findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("No feeds() method found on the Feeder class"));
-        assertThat(feedMethod.getParameterTypes()[0].getName()).isEqualTo("FoodType");
+        assertThat(feedMethod.getReturnType().getName()).isEqualTo("com.serenitydojo.model.FoodType");
     }
 }
